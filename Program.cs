c5d44acd34fa89b6af3bde041c36322b6d91db89
ESMERALDOJr <esmeraldo.log@hotmail.com>
2022-01-20 10:26:09 -0300

    Desafio2 DIO C#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Desafio2
+{
+    class DIO
+    {
+        static void Main(String[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n; i+=2)
+            {
+                int b = (int)Math.Pow(1, 2);
+                b = i * i;
+                Console.WriteLine($"{i}^2 = {b}");
+            }
+        }
+    }
+}
